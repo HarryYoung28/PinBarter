f92331e21485b5e707d821d4c64168b02ca9ebfa
HarryYoung <harryyoung1995@icloud.com>
2026-04-08 14:20:27 +0100

    supabase errors with vars attempt fix 1

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,13 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 
-// common pattern used for NextJS and prisma to prevent multiple connections during development
-
 const globalForPrisma = globalThis
-
-const prisma = globalForPrisma.prisma ?? new PrismaClient({
-    datasourceUrl: process.env.POSTGRES_PRISMA_URL + "?pgbouncer=true&connection_limit=1"
-})
-
+const prisma = globalForPrisma.prisma ?? new PrismaClient()
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
 export default prisma
